a9c8b625cb945dc7eba04ab2826bb9e3faeae70d
Peter Csukardi <peter@vermilion.tech>
2018-09-27 21:29:04 -0400

    Update ping_test.sh

--- ping_test.sh
+++ ping_test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-IP='10.10.17.1'
+
+#This is where the variable is set, and can be changed.
+IP='10.10.17.1' 
+
+#This redirects any output to a garvage file instead of the screen.
 ping $IP 2>/dev/null 1/dev/null
+
+#This if statement outputs a message to the screen based on ping results.
 if [ "$?" = 0 ]
 then
   echo "Host reachable"
