02a1ba161c6ba48eb047d4e9d0fd4876f1bc6701
pcsukardi <peter@vermilion.tech>
2018-09-28 12:25:59 -0400

    Made an edit to the ping_test script.

--- ping_test.sh
+++ ping_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #This is where the variable is set, and can be changed.
-IP='10.10.17.1' 
+IP='127.0.0.1' 
 
 #This redirects any output to a garvage file instead of the screen.
-ping $IP 2>/dev/null 1>/dev/null
+ping -c5 $IP > /dev/null
 
 #This if statement outputs a message to the screen based on ping results.
 if [ "$?" = 0 ]
