d9c336e83ca8d09f3a677146cfcc5ac2a2bee6bd
Peter Csukardi <peter@vermilion.tech>
2018-09-27 21:14:06 -0400

    This is a simple ping test script that is flexible.

--- ping_test.sh
+++ ping_test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-IP='45.79.158.194'
-ping $IP 2>/dev/null 1>/dev/null &
-
+IP='10.10.17.1'
+ping $IP 2>/dev/null 1/dev/null
 if [ "$?" = 0 ]
 then
-  echo "Host found"
+  echo "Host reachable"
 else
-  echo "Host not found"
+  echo "Host not reachable"
 fi  
