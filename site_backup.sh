efc989787a3f09f852bdfea116783069d1e15fff
Peter Csukardi <peter@vermilion.tech>
2018-12-05 16:15:13 -0500

    Update site_backup.sh

--- site_backup.sh
+++ site_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mysqldump -u wpuser -ppassword12 wordpress > /var/www/html/wordpress/"db_backup.sql.$(date)"
+mysqldump -u {userhere} -p{passwordhere} {databasehere} > /var/www/html/wordpress/"db_backup.sql.$(date)"
 
-rsync -azh /var/www/html/wordpress admin@173.220.39.125:/share/homes/admin/
+rsync -azh /var/www/html/wordpress {user@serveraddresshere}:/share/homes/admin/
 
 exit 0
