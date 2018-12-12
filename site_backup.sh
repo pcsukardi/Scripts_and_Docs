e6991994a27f41aecaa6f903d8e02b7244f2e1bb
pcsukardi <peter@vermilion.tech>
2018-12-11 19:58:45 -0500

    Fixed a syntax issue.

--- site_backup.sh
+++ site_backup.sh
@@ -6,7 +6,7 @@ Database={database_name}
 Path={directory_path_to_backup}
 Serveraddress={destination_ip_address}
 
-mysqldump -u $User -p$Password $Database > $Path/"db_backup.$(date +%Y%m%d).sql"
+mysqldump -u $User -p$Password $Database > $Path/"db_backup.$(date).sql"
 
 rsync -azh $Path user@$Serveraddress:/share/homes/admin/
 
